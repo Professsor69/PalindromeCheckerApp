80890e90675e1cc2c6faa6188c8fd205aa547f66
Kushagra Agarwal <kushagra458377@gmail.com>
2026-03-05 02:18:45 +0530

    UC2: Hardcoded palindrome check

--- src/main/java/PalindromeCheckerApp.java
+++ src/main/java/PalindromeCheckerApp.java
@@ -2,8 +2,9 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
+        // UC1 - Welcome Message
         System.out.println("=================================");
-        System.out.println("      PALINDROME CHECKER APP     ");
+        System.out.println("        PALINDROME CHECKER APP        ");
         System.out.println("=================================");
 
         System.out.println("Application Name : Palindrome Checker App");
@@ -12,5 +13,17 @@ public class PalindromeCheckerApp {
         System.out.println("Welcome to the Palindrome Checker Application!");
         System.out.println("The program will check whether a given string is a palindrome.");
 
+        System.out.println("\n------ UC2: Hardcoded Palindrome Check ------");
+
+        // UC2 - Hardcoded Palindrome
+        String word = "madam";
+
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if(word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
     }
 }
