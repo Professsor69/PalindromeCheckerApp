9fe9b326ce4f10ddf9b6177f4e42690c34bd2429
Kushagra Agarwal <kushagra458377@gmail.com>
2026-03-05 02:23:47 +0530

    UC4: Character array palindrome check using two pointer technique

--- src/main/java/PalindromeCheckerApp.java
+++ src/main/java/PalindromeCheckerApp.java
@@ -36,5 +36,29 @@ public class PalindromeCheckerApp {
         } else {
             System.out.println(original + " is NOT a Palindrome");
         }
+        System.out.println("\n--- UC4: Character Array Palindrome Check ---");
+
+        String text = "radar";
+        char[] arr = text.toCharArray();
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        boolean isPalindrome = true;
+
+        while(start < end) {
+            if(arr[start] != arr[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if(isPalindrome) {
+            System.out.println(text + " is a Palindrome");
+        } else {
+            System.out.println(text + " is NOT a Palindrome");
+        }
     }
 }
